b1bb51aed137e103980857da07a76b1c7bba51cd
Yiannis <ioannis.roussos@hotmail.com>
2025-09-19 13:24:49 +0300

    Modal done

--- index.js
+++ index.js
@@ -2,22 +2,24 @@
 
 function contact(event) {
   event.preventDefault();
-    //emailjs.sendForm(
-      //  'service_grcodes',
-        // 'template_ca6dzam',
-          //event.target,
-          //'LjuZS-A8QUs3HDnbb'
-       // ) .then(() => {
-        //    console .log('SUCCESS!');
-        //});
-
-        const loading = document.querySelector('.modal_overlay--loading')
+        const loading = document.querySelector('.modal__overlay--loading')
         const success = document.querySelector('.modal__overlay--success')
         loading.classList += " modal__overlay--visible"
-        setTimeout(() => {
+
+    emailjs
+        .sendForm(
+        'service_grcodes',
+         'template_ca6dzam',
+           event.target,
+          'LjuZS-A8QUs3HDnbb'
+        ) .then(() => {
             loading.classList.remove("modal__overlay--visible");
             success.classList += " modal__overlay--visible"
-            console.log('It worked')
-
-}, 1000);
-}
+           
+        }) .catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly at grcodes@outlook.com"
+            );
+        })
+      }
